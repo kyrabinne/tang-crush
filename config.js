5c0a0af3da9c29bd8791145799ffa94abec75b83
kyrabinne <86762716+kyrabinne@users.noreply.github.com>
2021-07-01 12:33:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Quần đùi à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao mày cứ làm tau mắc HỀ quá vậy hả
+    Lạc vào tim tui lẻ loi
+    Đằng sau chữ yêu đây là đập thấy mẹ mày h`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy cái mũi ấy :)) ',
+    btnYes: 'Vẫn cứ là thích kim tang <33',
+    btnNo: 'Không, mày hok đi tau đập mày h à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao mày làm tau cáu quài vậy :) <3',
+    btnReply: 'Gửi cho pé <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Haha tau biết mà quần đùi 🥰. Yêu trí kun nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 10h call nhá quần đùi.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+   
 }
